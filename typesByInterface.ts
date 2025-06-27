f385cd804b409d4ae0d14b6d015124a1b83dc491
bagoesrex <bagoesrex@gmail.com>
2025-06-27 21:10:04 +0700

    types by interface

---
+++ typesByInterface.ts
@@ -0,0 +1,15 @@
+// TypeScript knows the JavaScript language and will generate types for you in many cases.
+// For example in creating a variable and assigning it to a particular value,
+// TypeScript will use the value as its type.
+
+let helloWorld = "Hello World";
+
+console.log(helloWorld)
+
+// By understanding how JavaScript works, TypeScript can build a type-system that accepts 
+// JavaScript code but has types. This offers a type-system without needing to add extra 
+// characters to make types explicit in your code. That’s how TypeScript knows that helloWorld 
+// is a string in the above example.
+
+// You may have written JavaScript in Visual Studio Code, and had editor auto-completion. 
+// Visual Studio Code uses TypeScript under the hood to make it easier to work with JavaScript.
